70396dcd057632453bb012c39e6e9ecce81efe4b
Oren Zaharia <orenzah@aperio.ai>
2023-08-20 18:44:39 +0300

    wip2

--- ci/deployer.py
+++ ci/deployer.py
@@ -2,7 +2,10 @@ import argparse
 import logging
 from ci.ci_class import CI
 from ci.exec import run
-from ci.path import 
+from ci.path import GRAFANA_ROOT
+
+from ci.config import CONFIG
+
 class Deployer(CI):
     def __init__(self):
         self.name = "deployer"
@@ -14,21 +17,27 @@ class Deployer(CI):
         self.args = args
         
     def add_args(self, argparser: argparse.ArgumentParser):
-        argparser.add_argument("--deployer", help="Deploy to k8s", action="store_true")
+        deployer_group = argparser.add_argument_group("Deployer Group")        
+        deployer_group.add_argument("--deployer", help="Deploy to k8s", action="store_true")
+        deployer_group.add_argument("--template", help="Output templates", action="store_true")
         self.argparser = argparser
     
 
     def main(self):
         if self.args.deployer:
-            with open(f"{GRAFANA_ROOT}/influx_token.ignore", "r") as f:
-                grafana_token = f.read().strip()
+            
             # grafana_token
             values = {
                 "influxdb.username": "admin",
                 "influxdb.password": "password", # chagnge password after deployment
-                "grafana.datasources[0].secureJsonData.token": grafana_token
+                "grafana.datasources[0].secureJsonData.token": CONFIG["grafana"]["influxdb"]["token"],                
+                "global.common.path": CONFIG["nfs"]["path"],
+                "global.common.nfs_server": CONFIG["nfs"]["server"],
+                
             }        
             cmd = ["helm", "upgrade", "--install", "--namespace", "stock", "stock-trading", "."]
+            if self.args.template:
+                cmd = ["helm", "template", "--namespace", "stock", "stock-trading", ".", "--debug"]
             for v in values.keys():
                 cmd.append(f"--set {v}={values[v]}")
-            run(cmd, cwd="./stocks-trading")
+            run(cmd, cwd="./stocks/stocks-trading")

7d0961d7aa85585e2422cebdcf1198c08a201944
Oren Zaharia <orenzah@aperio.ai>
2023-08-29 15:56:22 +0300

    formmating

--- ci/exec.py
+++ ci/exec.py
@@ -1,28 +1,31 @@
-import os, sys
-import time
 import argparse
+import os
 import subprocess
+import sys
+import time
 
 from ci.logger import mylogger
+
 
 class RunOutput():
     def __init__(self, output: str, error: str):
         self.output = output
         self.error = error
+
     def __repr__(self):
         return f"Output: {self.output}\nError: {self.error}"
 
 
-def run(cmd: list[str], cwd: str = ".", env: dict = {}, quiet: bool = False):    
+def run(cmd: list[str], cwd: str = ".", env: dict = {}, quiet: bool = False):
     # cmd = " ".join(cmd)
-    # cmd = f"env -C {cwd} {cmd}"                        
+    # cmd = f"env -C {cwd} {cmd}"
     logger = mylogger.getLogger(__name__)
     new_env = os.environ.copy()
     new_env.update(env)
-    # cmd = " ".join(cmd)        
-    logger.info(f"env -C {env if env else ''}{cwd if cwd != './' else ''} {cmd}")        
+    # cmd = " ".join(cmd)
+    logger.info(f"env -C {env if env else ''}{cwd if cwd != './' else ''} {cmd}")
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, cwd=cwd, env=new_env)
-    (output, error) = proc.communicate()    
+    (output, error) = proc.communicate()
     if not quiet:
         for line in output.decode("utf-8").split("\n"):
             line = line.strip()
@@ -30,5 +33,3 @@ def run(cmd: list[str], cwd: str = ".", env: dict = {}, quiet: bool = False):
                 logger.info(line)
     output = RunOutput(output, error)
     return output
-
-    
--- ci/logger.py
+++ ci/logger.py
@@ -2,26 +2,25 @@ import logging
 import logging.config
 
 LOGGING_CONFIG = {
-    "version":1,
-    "root":{
-        "handlers" : ["console"],
+    "version": 1,
+    "root": {
+        "handlers": ["console"],
         "level": "DEBUG"
     },
-    "handlers":{
-        "console":{
+    "handlers": {
+        "console": {
             "formatter": "std_out",
             "class": "logging.StreamHandler",
             "level": "DEBUG"
         }
     },
-    "formatters":{
+    "formatters": {
         "std_out": {
             "format": '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-            "datefmt":"%d-%m-%Y %I:%M:%S"
+            "datefmt": "%d-%m-%Y %I:%M:%S"
         }
     },
 }
 
 mylogger = logging
 mylogger.config.dictConfig(LOGGING_CONFIG)
-

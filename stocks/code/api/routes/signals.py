cd8f75f7cc15f0e27fed26063805f5d3747b854e
Oren Zaharia <orenzah@aperio.ai>
2023-08-30 10:42:24 +0300

    remove algo and add date support

--- stocks/code/api/routes/signals.py
+++ stocks/code/api/routes/signals.py
@@ -4,9 +4,16 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+class QueryEnter(BaseModel):
+    symbol: str
+    entry_datetime: str # date format: 2021-01-01 00:00:00       
+    
 
-@router.get("/ShouldEnterPosition", tags=["signals"])
-async def should_enter_position(symbol: str):
+@router.post("/ShouldEnterPosition", tags=["signals"])
+async def should_enter_position(query: QueryEnter):
+    symbol = query.symbol
+    entry_datetime = query.entry_datetime
+    
     # TODO send request to the algo server to get the answer
     # answer is a boolean and timeout_seconds and stop_lose_price and exit_price
     pass
